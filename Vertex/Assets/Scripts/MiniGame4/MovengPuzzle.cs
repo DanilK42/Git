66de313410ac797b999b51eeb88a15120050b027
DanilK42 <danilkurusin87@gmail.com>
2025-02-17 20:03:08 +0500

    10 Comite
    Доделал мини игру с пинкодом

---
+++ Vertex/Assets/Scripts/MiniGame4/MovengPuzzle.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovengPuzzle : MonoBehaviour
+{
+    bool move;
+    Vector2 mousePos;
+    float startPosX;
+    float startPosY;
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            move = true;
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Конвертация координат
+            startPosX = mousePos.x - transform.position.x;
+            startPosY = mousePos.y - transform.position.y;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        move = false;
+    }
+
+    private void Update()
+    {
+        if (move)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Конвертация координат
+            transform.position = new Vector2(mousePos.x - startPosX, mousePos.y - startPosY);
+        }
+    }
+
+}
---
+++ Vertex/Assets/Scripts/MiniGame4/StartGame4.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame4 : MonoBehaviour
+{
+    public Camera cam;
+    public Transform scenCam;
+    public Transform gameCam;
+    public Animator anima;
+    public float time;
+    public GameObject checkGame;
+    public GameObject dialog;
+    private bool isPlayerInTrigger = false;
+    public bool clouseGame ;
+
+
+    private void Start()
+    {
+        checkGame.SetActive(true);
+        clouseGame = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && clouseGame == true)
+        {
+            Move.muv = true;
+            anima.SetTrigger("BlackWind");
+            Invoke("Teleport2", time);
+            clouseGame = false;
+        }
+
+        if (isPlayerInTrigger && Input.GetKeyDown(KeyCode.E) && clouseGame == false)
+        {
+            Collider2D targetCollider = checkGame.GetComponent<Collider2D>();
+            targetCollider.enabled = false;
+            Move.muv = false;
+            anima.SetTrigger("BlackWind");
+            Invoke("Teleport", time);
+            clouseGame = true;
+        }
+    }
+
+    void Teleport()
+    {
+        cam.transform.position = gameCam.position;
+    }
+    void Teleport2()
+    {
+        cam.transform.position = scenCam.position;
+        checkGame.GetComponent<Collider2D>().enabled = true; // ¬ключаем коллайдер обратн
+    }
+}
